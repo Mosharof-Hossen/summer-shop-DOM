97111240dcd537c10635ed88d9d35ab74f9dc93a
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-06-26 00:15:17 +0600

    all buy button worked

--- JS/summerShop.js
+++ JS/summerShop.js
@@ -5,3 +5,59 @@ document.getElementById("k-accessories-btn").addEventListener('click', () => {
     setProductName(itemName);
     setTotalPrice(itemPrice);
 })
+document.getElementById("K-Wood-Accessories-btn").addEventListener('click', () => {
+    let itemName = getValueByID("K-Wood-Accessories");
+    let itemPrice = parseFloat(getValueByID("K-Wood-Accessories-price")).toFixed(2);
+    console.log(itemName, itemPrice);
+    setProductName(itemName);
+    setTotalPrice(itemPrice);
+})
+document.getElementById("Home-Cooker-btn").addEventListener('click', () => {
+    let itemName = getValueByID("Home-Cooker");
+    let itemPrice = parseFloat(getValueByID("Home-Cooker-price")).toFixed(2);
+    console.log(itemName, itemPrice);
+    setProductName(itemName);
+    setTotalPrice(itemPrice);
+})
+document.getElementById("Sports-Back-Cap-btn").addEventListener('click', () => {
+    let itemName = getValueByID("Sports-Back-Cap");
+    let itemPrice = parseFloat(getValueByID("Sports-Back-Cap-price")).toFixed(2);
+    console.log(itemName, itemPrice);
+    setProductName(itemName);
+    setTotalPrice(itemPrice);
+})
+document.getElementById("Full-Jersey-Set-btn").addEventListener('click', () => {
+    let itemName = getValueByID("Full-Jersey-Set");
+    let itemPrice = parseFloat(getValueByID("Full-Jersey-Set-price")).toFixed(2);
+    console.log(itemName, itemPrice);
+    setProductName(itemName);
+    setTotalPrice(itemPrice);
+})
+document.getElementById("Sports-cates-btn").addEventListener('click', () => {
+    let itemName = getValueByID("Sports-cates");
+    let itemPrice = parseFloat(getValueByID("Sports-cates-price")).toFixed(2);
+    console.log(itemName, itemPrice);
+    setProductName(itemName);
+    setTotalPrice(itemPrice);
+})
+document.getElementById("Single-Relax-Chair-btn").addEventListener('click', () => {
+    let itemName = getValueByID("Single-Relax-Chair");
+    let itemPrice = parseFloat(getValueByID("Single-Relax-Chair-price")).toFixed(2);
+    console.log(itemName, itemPrice);
+    setProductName(itemName);
+    setTotalPrice(itemPrice);
+})
+document.getElementById("Children-Play-btn").addEventListener('click', () => {
+    let itemName = getValueByID("Children-Play");
+    let itemPrice = parseFloat(getValueByID("Children-Play-price")).toFixed(2);
+    console.log(itemName, itemPrice);
+    setProductName(itemName);
+    setTotalPrice(itemPrice);
+})
+document.getElementById("Flexible-Sofa-btn").addEventListener('click', () => {
+    let itemName = getValueByID("Flexible-Sofa");
+    let itemPrice = parseFloat(getValueByID("Flexible-Sofa-price")).toFixed(2);
+    console.log(itemName, itemPrice);
+    setProductName(itemName);
+    setTotalPrice(itemPrice);
+})
