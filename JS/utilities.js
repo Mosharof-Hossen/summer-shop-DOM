f432e160c98f50c2970c32515aa52c54469969ee
Mosharof-Hossen <mosharofhossen1998@gmail.com>
2024-06-26 01:49:21 +0600

    coupon added

--- JS/utilities.js
+++ JS/utilities.js
@@ -5,8 +5,7 @@ function getValueByID(ID) {
 function setProductName(product) {
     let parent = document.getElementById('card-item-list');
     let p = document.createElement("p");
-    console.log(parent.childElementCount);
-    p.classList.add('text-xl','font-medium');
+    p.classList.add('text-xl', 'font-medium');
     p.innerHTML = `
     ${parent.childElementCount + 1}. ${product}
     `
@@ -18,4 +17,32 @@ function setTotalPrice(price) {
     let totalPrice = parseFloat(getValueByID('total-price')).toFixed(2);
     let finalPrice = parseFloat(parseFloat(totalPrice) + parseFloat(price)).toFixed(2);
     totalPriceElement.innerText = finalPrice;
+    document.getElementById('total').innerText = finalPrice;
+}
+
+function buttonActivator() {
+    let totalPriceElement = document.getElementById("card-item-list");
+    let totalPrice = getValueByID('total-price');
+    let makePurchase = document.getElementById("make-purchase");
+    let applyBtn = document.getElementById('apply');
+    if (totalPriceElement.childElementCount > 0) {
+        makePurchase.classList.remove('btn-disabled');
+    }
+    if (parseInt(totalPrice) > 200) {
+        applyBtn.classList.remove("btn-disabled");
+    }
+}
+
+function discountApply() {
+    let totalPrice = parseFloat(getValueByID('total-price'));
+    let coupon = document.getElementById('coupon-input').value;
+
+    if (coupon === 'SELL200') {
+        let discount = (20 * totalPrice) / 100;
+        document.getElementById('discount').innerText = parseFloat(discount).toFixed(2);
+        document.getElementById('total').innerText = parseFloat(totalPrice - discount).toFixed(2);
+    } else {
+        alert("Wrong Coupon. Coupon is: SELL200")
+    }
+
 }
